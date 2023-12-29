83ede4b81ebdcf13eae63073005c0247d06adcbc
Angelo Torella <angelotorella2002@gmail.com>
2023-12-28 22:45:22 -0400

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,52 @@
+import {  IonHeader,  IonTitle, IonToolbar, 
+  IonPage,
+  IonContent,
+  IonInput,
+  IonButton,
+  IonCard,
+  IonCardHeader,
+  IonCardContent,
+  IonCardTitle,
+  IonLabel,
+  IonItem,} from '@ionic/react';
+import './css/Home.css';
+  
+import MonitoringView from '../View/MonitoringView';
+import GeolocationView from '../View/GeolocationView';
+import MotionView from '../View/MotionView';
+
+const Home: React.FC = () => {
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Attacker App</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen >
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Attacker App</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+
+        <MonitoringView/>
+
+        
+        <IonCard>
+          <IonCardHeader>
+              <IonCardTitle>Título de la Tarjeta</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>              
+            <GeolocationView/>
+            <MotionView/>
+          </IonCardContent>
+        </IonCard>
+
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
