3501f59d70dc59db932e02c0f1e7a2c220aa6c22
Angelo Torella <angelotorella2002@gmail.com>
2024-01-10 14:28:28 -0400

    cambios en la peticion de monitoreo

--- src/Remote/MotionRemote.tsx
+++ src/Remote/MotionRemote.tsx
@@ -3,28 +3,36 @@ import { BaseURL } from './BaseURL';
 import { LoginModel } from '../Model/LoginModel';
 
 export class MotionRemote {
-
-
     public async sendMotion(isMoving: boolean, timeStamp: Date) {
         const username = LoginModel.getInstance().getUsername();
 
+        const movimientoData = [
+            {
+              isMoving: isMoving,
+              fecha: timeStamp
+            }
+          ];
+          
+            console.log("movimientoData", movimientoData);
         try {
-            const response = await axios.post(`${BaseURL.baseUrl}/users/setMovimiento`, {
-                username: username,
-                movimiento: [
-                    {
-                        isMoving: isMoving,
-                        currenTime: timeStamp
-                    }
-                ]
-            },{timeout: 5000});
+            const response = await fetch(`${BaseURL.baseUrl}/users/setMovimiento/${username}`, {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(movimientoData)
+            });
 
-            return response.data;
+            console.log("response, ", response);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const responseData = await response.json();
+            return responseData;
         } catch (error) {
-            if (axios.isAxiosError(error)) {
-                if (error.code === 'ECONNABORTED') {
-                    throw { code: 'TIMEOUT_ERROR', message: 'La solicitud tardó demasiado tiempo, por favor verifica tu conexión a internet' };
-                }
+            if ((error as Error).name === 'AbortError') {
+                throw { code: 'TIMEOUT_ERROR', message: 'La solicitud tardó demasiado tiempo, por favor verifica tu conexión a internet' };
             }
             throw new Error('Error de red o del servidor');
         }
